dfc8e9a7e3adc9ccec2452df88b9f9b691aa379b
Pchabai <chabaipaul@gmail.com>
2025-02-06 15:29:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-red-300 p-10">
+    {console.log("Projects:", projects)}
       <h1 className="text-3xl font-bold text-center mb-6">Project Dashboard</h1>
 
       {/* Grid Layout for Projects */}
