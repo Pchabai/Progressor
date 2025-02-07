2485f39e6c40f0115ba9512533a6afbbbcc04111
Pchabai <chabaipaul@gmail.com>
2025-02-06 21:37:21 -0500

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLT9O-1GqWjfyjfSF8m0vB5dnePE1rMg0",
+  authDomain: "progressor-35b8b.firebaseapp.com",
+  projectId: "progressor-35b8b",
+  storageBucket: "progressor-35b8b.firebasestorage.app",
+  messagingSenderId: "981817094608",
+  appId: "1:981817094608:web:93561756fe51ccaad489d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
